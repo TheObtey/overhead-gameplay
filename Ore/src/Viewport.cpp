b4aae318ed2e1bcbaece05eb759311a36723c193
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2026-03-05 10:50:15 +0100

    [ADD] Viewport

---
+++ Ore/src/Viewport.cpp
@@ -0,0 +1,56 @@
+#include "Viewport.h"
+#include "Logger.hpp"
+#include "Window.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+Viewport::Viewport(uint16 x, uint16 y, uint16 width, uint16 height, Color const& backgroundColor, Window& window)
+{
+    m_x = x;
+    m_y = y;
+    m_width = width;
+    m_height = height;
+    m_backgroundColor = backgroundColor;
+    m_pWindow = std::make_shared<Window>(window);
+
+    Display();
+}
+
+Viewport::~Viewport()
+{
+}
+
+void Viewport::SetSize(uint16 width, uint16 height)
+{
+    m_width = width;
+    m_height = height;
+}
+
+void Viewport::Display()
+{
+    glfwMakeContextCurrent(m_pWindow.get()->Get());
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		Logger::LogWithLevel(LogLevel::ERROR, "Failed to initialize GLAD");
+		return;
+	}
+
+    glViewport(m_x, m_y, m_width, m_height);
+}
+
+void Viewport::Clear()
+{
+    glfwMakeContextCurrent(m_pWindow.get()->Get());
+
+    glEnable(GL_SCISSOR_TEST);
+    glScissor(m_x, m_y, m_width, m_height);
+    glClearColor(m_backgroundColor.r, m_backgroundColor.g, m_backgroundColor.b, m_backgroundColor.a);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glDisable(GL_SCISSOR_TEST);
+}
+
+void Viewport::Present()
+{
+}
