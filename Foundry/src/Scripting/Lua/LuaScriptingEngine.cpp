19aec40ef17318b0e6fb334b885f54c35cef9918
Mithaniel Villard <mvillardhernandez@gaming.tech>
2026-03-13 16:34:13 +0100

    [ADD] Lua hook and timer library

--- Foundry/src/Scripting/Lua/LuaScriptingEngine.cpp
+++ Foundry/src/Scripting/Lua/LuaScriptingEngine.cpp
@@ -3,5 +3,11 @@
 ScriptingEngine::ScriptingEngine() { OnInitialize(); }
 ScriptingEngine::~ScriptingEngine() { OnUninitialize(); }
 
-void ScriptingEngine::OnInitialize() { m_scriptEngine.open_libraries(sol::lib::base, sol::lib::package, sol::lib::debug, sol::lib::os); }
+void ScriptingEngine::OnInitialize()
+{
+    m_scriptEngine.open_libraries(sol::lib::base, sol::lib::package, sol::lib::debug, sol::lib::os);
+    m_scriptEngine.script_file("res/.foundry/hook.lua");
+    m_scriptEngine.script_file("res/.foundry/timer.lua");
+}
+
 void ScriptingEngine::OnUninitialize() { m_scriptEngine.collect_garbage(); }
