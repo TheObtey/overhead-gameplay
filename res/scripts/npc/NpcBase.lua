ffde0f806fb4db04264b435e57350787fc753645
TheObtey <95309102+TheObtey@users.noreply.github.com>
2026-04-24 17:07:26 +0200

    [ADD] NPC base, and Host NPC

---
+++ res/scripts/npc/NpcBase.lua
@@ -0,0 +1,52 @@
+local sNpcName = "NPC"
+local bCanTalk = true
+
+function self:SetNPCName(sNewName)
+    sNpcName = sNewName
+end
+
+function self:GetNPCName()
+    return sNpcName
+end
+
+function self:SetCanTalk(bNewCanTalk)
+    bCanTalk = bNewCanTalk
+end
+
+function self:CanTalk()
+    return bCanTalk
+end
+
+function self:CanInteract()
+    return self:CanTalk()
+end
+
+function self:Interact(oInteractor)
+    if not oInteractor then
+        return
+    end
+
+    if not bCanTalk then
+        print(sNpcName.." doesn't want to talk right now")
+        return
+    end
+
+    self:OnInteract(oInteractor)
+end
+
+function self:OnInteract(oInteractor)
+    print("Talking with "..sNpcName)
+end
+
+function self:GetPrompt()
+    return "Talk with "..sNpcName
+end
+
+function OnInit()
+end
+
+function OnUpdate(iDeltaTime)
+end
+
+function OnDetroy()
+end
