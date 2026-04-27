a03f4dd02437d6c428a5d8da219393f556017421
TheObtey <95309102+TheObtey@users.noreply.github.com>
2026-04-27 21:51:16 +0200

    [REF] Use trigger state to finish gravity field system

--- res/scripts/GravityField.lua
+++ res/scripts/GravityField.lua
@@ -1,6 +1,11 @@
 local vecGravDirection = fmath.vec3:new(0, 1, 0)
 local oCollider
 
+local TriggerState = {
+    ENTERED = 1,
+    EXITED = 2
+}
+
 local function GetGravityReceiverFromBody(oBody)
     if not oBody then
         return nil
@@ -14,12 +19,21 @@ local function GetGravityReceiverFromBody(oBody)
     return oCompContainer:FindChild("GravityReceiverComponent")
 end
 
-local function OnTrigger(collider, rigidbody)
-    if not rigidbody then return end
+local function OnBodyEntered(oRigidbody)
+    if not oRigidbody then return end
 
-    local oReceiver = GetGravityReceiverFromBody(rigidbody)
+    local oReceiver = GetGravityReceiverFromBody(oRigidbody)
     if oReceiver and oReceiver.ReceiveGravityFromField then
         oReceiver:ReceiveGravityFromField(self, vecGravDirection)
+    end
+end
+
+local function OnBodyExited(oRigidbody)
+    if not oRigidbody then return end
+
+    local oReceiver = GetGravityReceiverFromBody(oRigidbody)
+    if oReceiver and oReceiver.ReceiveGravityFromField then
+        oReceiver:ClearGravityFromField(self)
     end
 end
 
@@ -29,8 +43,12 @@ function OnInit()
         oCollider = oCollider:As(NodeTypes.NODE_COLLIDER)
         oCollider:SetIsTrigger(true)
         oCollider:SetIsWorldQueryCollider(false)
-        oCollider:SubscribeOnTrigger(function(collider, rigidbody)
-            OnTrigger(collider, rigidbody)
+        oCollider:SubscribeOnTrigger(function(_, oRigidbody, iState)
+            if iState == TriggerState.ENTERED then
+                OnBodyEntered(oRigidbody)
+            elseif iState == TriggerState.EXITED then
+                OnBodyExited(oRigidbody)
+            end
         end)
     end
 end
